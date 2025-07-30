594957255aa40966165fc69c54faae0ac8a663c4
thegaboec <gabrielguerrero38@gmail.com>
2025-07-30 17:19:16 -0500

    ejercicio 1.5

--- Part-1/src/App.jsx
+++ Part-1/src/App.jsx
@@ -1,10 +1,11 @@
 
 
-const Header = (props) =>{
+const Header = ({courseName}) =>{
+  console.log({courseName})
   return (
     <div>
-      console.log(props)
-      <p> The course you are enrolled in is : {props.course}</p>
+      
+      <p> The course you are enrolled in is : {courseName}</p>
     </div>
      
   )
@@ -14,14 +15,9 @@ const Content =({parts}) => {
 
   return (
     <div>
-      <p>
-        The Content to will you see is : 
-      </p>
-      < Part part ={parts[0]}/>
-      < Part part ={parts[1]}/>
-      < Part part ={parts[2]}/>
-      
-     
+      {parts.map((part) => (
+        <Part key={part.name} part={part} />
+      ))}
     </div>
   )
 
@@ -59,29 +55,36 @@ const Part =({part}) => {
 
 const App = () => {
 
-  const course = 'Half Stack application development'
+ 
  
 
-  const parts =[
-    {
-      name:'Fundamentals of React',
-      exercises:10},
-    {
-      name:'Using props to pass data',
-      exercises:7},
-    {
-      name:'State of a component',
-      exercises:14},
-  ]
+  const course ={
 
+    name:'Half Stack application development',
+    parts: [
+      {
+        name:'Fundamentals of React',
+        exercises:10
+      },
+      {
+        name:'Using props to pass data',
+        exercises:7
+      },
+      {
+        name:'State of a component',
+        exercises:14
+      }
+
+    ]
   
+  }
 
   return (
     <div>
       
-      <h1><Header course= {course}/></h1>
-      <Content parts= {parts}/>
-      <Total parts= {parts}/>
+      <h1><Header courseName= {course.name}/></h1>
+      <Content parts= {course.parts}/>
+      <Total parts= {course.parts}/>
       
     </div>
   )
