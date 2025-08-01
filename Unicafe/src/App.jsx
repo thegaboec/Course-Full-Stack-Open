0809a262edd96036eed853c2e5f1cf502d692a7d
thegaboec <gabrielguerrero38@gmail.com>
2025-08-01 17:13:12 -0500

    Unicafe 1.11

--- Unicafe/src/App.jsx
+++ Unicafe/src/App.jsx
@@ -86,7 +86,7 @@ const App = () => {
 
   return (
     <div>
-      code here
+      
 
       < Titulo titulo={feedbackTitle}/>
       < Button handleClick={ () =>handleFeedbackClick('good')} text='good'/>
